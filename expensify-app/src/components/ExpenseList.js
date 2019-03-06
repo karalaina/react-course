ec77bd019e2dd44a8c8b609bd49ebb00820254ee
unknown <karacampbell@ICOS-campbka.cloud.statcan.ca>
2019-03-06 16:39:09 -0500

    Completed up to lecture 102

--- expensify-app/src/components/ExpenseList.js
+++ expensify-app/src/components/ExpenseList.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 const ExpenseList = (props) => (
     <div>
         <h1>Expense List</h1>
-        {props.filters.text}
         {props.expenses.length}
     </div>
 );
